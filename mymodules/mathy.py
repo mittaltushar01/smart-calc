beb8c6944d7f028a3e0d9877019de0042930b894
Tushar <mittaltushar.all@gmail.com>
2019-09-16 12:22:25 +0530

    Smart calc program

---
+++ mymodules/mathy.py
@@ -0,0 +1,53 @@
+responses=["Welcome! To the smart calculator","My name is - Michael Scott. (You probably remember me as the regional manager of Dunder Mifflin)", "Thanks", "Sorry! You are tricking me to compute beyound my ability (I will transfer this work to Dwight Schrute)"]
+
+def extract_numbers_from_text(text):
+	l=[]
+	for t in text.split(' '):
+		try:
+			l.append(float(t))
+		except ValueError:
+			pass
+	return (l)
+
+def lcm(a,b):
+	L=a if a>b else b
+	while L<=a*b:
+		if L%a==0 and L%b==0:
+			return L
+	L+=1
+
+def hcf(a,b):
+	H=a if a<b else b
+	while H>=1:
+		if a%H==0 and b%H==0:
+			return H
+		H-=1
+
+def add(a,b):
+	return a+b
+
+def sub(a,b):
+	return a-b
+
+def multiply(a,b):
+	return a*b
+
+def division(a,b):
+	return a/b
+
+def end():
+	print(responses[2])
+	input("Please enter key to exit - Thanks from Michael Scott")
+	exit()
+
+def myname():
+	print(responses[1])
+
+def sorry():
+	print(responses[3])
+ 
+operations={"PLUS": add,"ADD":add,"SUM":add,"MINUS":sub,"SUBTRACTION":sub,"SUBTRACT":sub,"SUB":sub,"HCF":hcf,"DIFFERENCE":sub,"MULTIPLY":multiply,"LCM":lcm,"DIVIDE":division,"DIVISION":division}
+commands={"NAME":myname,"END":end,"EXIT":end,"CLOSE":end}
+
+
+ 
